3f5a3b3c033f8ca5c6a1659f2fcb229e7c2e6321
hemanthbabu648 <hemanth170648@gmail.com>
2025-06-20 10:09:19 +0530

    (feat) added UI for experience and social navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,30 @@
 import About from '@/components/home/About';
+import Experiences from '@/components/home/Experiences';
 import Hero from '@/components/home/Hero';
+import { socialIcons } from '@/constants/home';
 
 function Home() {
   return (
     <>
       <Hero />
       <About />
+      <div className="hidden fixed top-[30%] right-0 w-16 h-80 z-30 bg-green-500 rounded-l-2xl sm:flex items-center">
+        <div className="space-y-4 flex flex-col items-center w-full">
+          {socialIcons.map(({ title, url, icon: Icon }) => (
+            <a
+              key={title}
+              href={url}
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label={title}
+              className="p-2 rounded-full bg-white hover:bg-gray-200 transition-all"
+            >
+              <Icon size={30} className="text-green-800" />
+            </a>
+          ))}
+        </div>
+      </div>
+      <Experiences />
     </>
   );
 }
--- src/constants/home.tsx
+++ src/constants/home.tsx
@@ -1,4 +1,15 @@
-import { Laptop2, PenLine, Search, Smartphone } from 'lucide-react';
+// TODO: Have to find package for icons that have brands or generate svgs for social icons
+import {
+  Laptop2,
+  PenLine,
+  Search,
+  Smartphone,
+  Linkedin,
+  Instagram,
+  Twitter,
+  Facebook,
+  Send,
+} from 'lucide-react';
 
 type Service = {
   title: string;
@@ -24,103 +35,91 @@ export const services: Service[] = [
   },
 ];
 
-export const socialIcons = [
+type SocialNav = {
+  title: string;
+  url: string;
+  icon: React.ElementType;
+};
+
+export const socialIcons: SocialNav[] = [
   {
-    title: 'Linked In',
+    title: 'LinkedIn',
     url: 'https://www.linkedin.com/in/hemanthbabu648/',
-    network: 'linkedin',
+    icon: Linkedin,
   },
   {
     title: 'Instagram',
     url: 'https://www.instagram.com/hemanthbabu648/',
-    network: 'instagram',
+    icon: Instagram,
   },
   {
     title: 'Twitter',
     url: 'https://twitter.com/hemanthbabu648',
-    network: 'twitter',
+    icon: Twitter,
   },
   {
     title: 'Facebook',
-    url: '',
-    network: 'facebook',
+    url: 'https://twitter.com/hemanthbabu648',
+    icon: Facebook,
   },
   {
     title: 'Telegram',
     url: 'https://t.me/s-hemanthbabu-648',
-    network: 'telegram',
+    icon: Send,
   },
 ];
 
-// export const technologies = [
-//   {
-//     name: "HTML 5",
-//     icon: html,
-//     value: 80
-//   },
-//   {
-//     name: "CSS 3",
-//     icon: css,
-//     value: 80
-//   },
-//   {
-//     name: "JavaScript",
-//     icon: javascript,
-//     value: 75
-//   },
-//   {
-//     name: "TypeScript",
-//     icon: typescript,
-//     value: 100
-//   },
-//   {
-//     name: "React JS",
-//     icon: reactjs,
-//     value: 75
-//   },
-//   {
-//     name: "Next JS",
-//     icon: nextjs,
-//     value: 75
-//   },
-//   {
-//     name: "Redux Toolkit",
-//     icon: redux,
-//     value: 75
-//   },
-//   {
-//     name: "Bootstrap",
-//     icon: bootstrap,
-//     value: 100
-//   },
-//   {
-//     name: "Tailwind CSS",
-//     icon: tailwind,
-//     value: 80
-//   },
-//   {
-//     name: "PHP",
-//     icon: php,
-//     value: 75
-//   },
-//   {
-//     name: "My SQL",
-//     icon: mysql,
-//     value: 75
-//   },
-//   {
-//     name: "git",
-//     icon: git,
-//     value: 75
-//   },
-//   {
-//     name: "figma",
-//     icon: figma,
-//     value: 80
-//   },
-//   // {
-//   //   name: "docker",
-//   //   icon: docker,
-//   //   value: 100
-//   // },
-// ];
+export type Experience = {
+  title: string;
+  company_name: string;
+  icon: string;
+  iconBg: string;
+  date: string;
+  points: string[];
+};
+
+export const experiences: Experience[] = [
+  {
+    title: 'Front-end Developer',
+    company_name: 'Bytup Technologies Pvt Ltd',
+    icon: '/images/companies/bytup.png',
+    iconBg: '#E6DEDD',
+    date: 'Feb 2025 - Present',
+    points: [
+      'Developed AI-driven mock interview platform (PrepAce) using Next.js, Tailwind CSS, Redux Toolkit, Server Actions, and Generative APIs.',
+      'Implemented advanced feedback mechanisms and integrated video recording features for enhanced user analysis.',
+      'Integrated real-time analytics and performance insights to improve user progress tracking.',
+      "Led the development of 'Any UNI', a university discovery platform with SEO-optimized routing, mentorship programs, and real-time application tracking.",
+      'Collaborated closely with designers and back-end teams to ship scalable and accessible features.',
+      'Actively participated in code reviews to ensure clean, maintainable code across projects.',
+    ],
+  },
+  {
+    title: 'Junior Front-end Developer',
+    company_name: 'Bytup Technologies Pvt Ltd',
+    icon: '/images/companies/bytup.png',
+    iconBg: '#383E56',
+    date: 'Aug 2023 - Jan 2025',
+    points: [
+      'Built admin and agent dashboards for a property management system (Real) using React, Tailwind CSS, Redux Toolkit, and TanStack Query.',
+      'Implemented real-time inquiry tracking, listing management, and performance analytics with Open APIs.',
+      'Contributed to core administrative tools such as user roles, system settings, and moderation features.',
+      'Wrote unit and integration tests using Jest to maintain component reliability and app performance.',
+      'Participated in sprint planning and QA sessions to improve feature delivery cycles.',
+    ],
+  },
+  {
+    title: 'Trainee Front-end Developer',
+    company_name: 'Bytup Technologies Pvt Ltd',
+    icon: '/images/companies/bytup.png',
+    iconBg: '#E6DEDD',
+    date: 'Jan 2023 - July 2023',
+    points: [
+      'Contributed to the development of an E-learning platform using Next.js, Tailwind CSS, and Strapi CMS.',
+      'Implemented dashboards with personalized learning progress tracking and course management features.',
+      'Worked on course discovery UX by integrating advanced filtering and search functionality.',
+      'Collaborated with the design and back-end teams to deliver pixel-perfect, mobile-first responsive components.',
+      'Learned version control, component structure, and responsive design fundamentals in real-world projects.',
+    ],
+  },
+];
